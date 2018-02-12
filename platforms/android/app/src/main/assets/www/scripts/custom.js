d1f24464949e5707519be334ff738c5c6f0a8783
Nimesh khatri <nimeshk@itpathsolutions.co.in>
2018-02-12 19:33:36 +0530

    Added UI change on whole app.

--- platforms/android/app/src/main/assets/www/scripts/custom.js
+++ platforms/android/app/src/main/assets/www/scripts/custom.js
@@ -1026,6 +1026,8 @@ $(document).ready(function () {
 })
 
 var api_baseUrl = 'http://hivehelperapp.com/appv2/v1/';
+//var api_baseUrl = 'http://hivehelper.project-demo.info/v1/';
+
 // detect mobile devices
 function isMobile() {
     var app = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
